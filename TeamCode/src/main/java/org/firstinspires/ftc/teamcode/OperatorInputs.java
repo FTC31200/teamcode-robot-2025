a89077a306289d8cde4653b8a5dd81ac95ea5522
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2025-11-05 14:15:23 -0300

    Create OperatorInputs.java
    Inputs & robot variables

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OperatorInputs.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class OperatorInputs {
+    public Gamepad gamepad;
+    public Telemetry telemetry;
+    private boolean prevRightTrigger = false;
+    private boolean prevLeftTrigger = false;
+    public boolean intakeToggle = false;
+    public double shooterSpeed = 1500.0;
+    public boolean isShooterOn = false;
+    public boolean shootActive = false;
+    public double drive, turn;
+
+    public OperatorInputs(Gamepad gamepad, Telemetry telemetry) {
+        this.gamepad = gamepad;
+        this.telemetry = telemetry;
+    }
+
+    public void update() {
+        drive = -gamepad.left_stick_y * 0.72;
+        turn = gamepad.right_stick_x * 0.85;
+
+        if (gamepad.left_trigger > 0.05 && !prevLeftTrigger) intakeToggle = !intakeToggle;
+        if (gamepad.right_trigger > 0.05 && !prevRightTrigger) shootActive = !shootActive;
+
+        prevLeftTrigger = gamepad.left_trigger > 0.05;
+        prevRightTrigger = gamepad.right_trigger > 0.05;
+
+        if (gamepad.dpadDownWasPressed()) isShooterOn = !isShooterOn;
+
+        // just for tweaks during tests
+        if (gamepad.dpadLeftWasPressed()) shooterSpeed -= 50.0;
+        if (gamepad.dpadRightWasPressed()) shooterSpeed += 50.0;
+        shooterSpeed = Math.max(600.0, Math.min(2850.0, shooterSpeed));
+    }
+}
